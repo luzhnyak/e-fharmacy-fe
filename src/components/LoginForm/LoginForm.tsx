3a4b7f0995f9e2d92a955f548f6dd5f7d50617ba
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-06-08 02:06:08 +0300

    add toast for incorect login

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -6,10 +6,19 @@ import css from './LoginForm.module.css';
 import Icon from '../Icon';
 import { authApi } from '../../redux/auth/authApi';
 import { LoginUser } from '../../types';
+import { ToastContainer, toast } from 'react-toastify';
+import Loader from '../Loader/Loader';
 
 const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [loginUser] = authApi.useLoginMutation();
+  const [loginUser, { error, isLoading }] = authApi.useLoginMutation();
+
+  useEffect(() => {
+    if (error) {
+      toast.error('The password or login is incorrect');
+      console.log('The password or login is incorrect');
+    }
+  }, [error]);
 
   const schema = yup
     .object({
@@ -32,39 +41,47 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className={css.wrap}>
-        <input
-          {...register('email')}
-          className={css.input}
-          placeholder="Email address"
-        />
-        <p className={css.errormessage}>{errors.email?.message}</p>
-
-        <div className={css.inputWrap}>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={css.wrap}>
           <input
-            {...register('password')}
+            {...register('email')}
             className={css.input}
-            placeholder="Password"
-            type={showPassword ? 'text' : 'password'}
+            placeholder="Email address"
           />
-          {showPassword ? (
-            <div className={css.icon} onClick={() => setShowPassword(false)}>
-              <Icon name="openeye" />
-            </div>
-          ) : (
-            <div className={css.icon} onClick={() => setShowPassword(true)}>
-              <Icon name="closeeye" />
-            </div>
-          )}
-        </div>
-        <p className={css.errormessage}>{errors.password?.message}</p>
-      </div>
+          <p className={css.errormessage}>{errors.email?.message}</p>
 
-      <button type="submit" className={css.button}>
-        Log in
-      </button>
-    </form>
+          <div className={css.inputWrap}>
+            <input
+              {...register('password')}
+              className={css.input}
+              placeholder="Password"
+              type={showPassword ? 'text' : 'password'}
+            />
+            {showPassword ? (
+              <div className={css.icon} onClick={() => setShowPassword(false)}>
+                <Icon name="openeye" />
+              </div>
+            ) : (
+              <div className={css.icon} onClick={() => setShowPassword(true)}>
+                <Icon name="closeeye" />
+              </div>
+            )}
+          </div>
+          <p className={css.errormessage}>{errors.password?.message}</p>
+        </div>
+
+        <button type="submit" className={css.button}>
+          Log in
+        </button>
+      </form>
+      {isLoading && <Loader />}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={true}
+      />
+    </>
   );
 };
 
